53c0754045904d3dad694f1fac536d9f831bc903
katlego kgotse <st10208619@rcconnect.edu.za>
2023-05-23 13:57:30 +0000

    This is what we did 23-05-23

---
+++ Generics/List/List/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> myList = new List<int>() { 5, 15 };
+        int[] numbers = { 16, 25, 30 };
+        int num;
+
+        //Error handling
+        try
+        {
+            Console.Write("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+            myList.Add(num);
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (OverflowException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+           // Console.WriteLine("!!-I didn't crash-!!");
+        }
+
+        //linq query 
+        var copyList = from item in myList
+                       select item;
+
+        //displaying all the information inside the list
+        foreach (var list in myList)
+        {
+            Console.WriteLine(list);
+        }
+        Console.WriteLine("===============COPY===============");
+        foreach (var items in copyList)
+        {
+            Console.WriteLine(items);
+        }
+    }
+}
---
+++ Non-Generics/Generics/Generics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ArrayList myArrList = new ArrayList();
+        int[] numbers = { 8, 9, 10 };
+
+        //Adding data into an array list
+        myArrList.Add("Sydney");
+        myArrList.Add(4.5);
+        myArrList.Add(false);
+        myArrList.AddRange(numbers); //allows to add all the information in another array. 
+
+        //Inserting data into an array list
+        myArrList.Insert(1, "Thato");
+        myArrList.InsertRange(0, numbers);
+
+        //Removing data from an array list
+        myArrList.Add(8);
+        myArrList.Remove(8);
+        myArrList.RemoveAt(5);
+
+        for (int i = 0; i < myArrList.Count; i++)
+        {
+            Console.WriteLine(myArrList[i]);
+        }
+
+        Console.WriteLine("+++++++++++++++++++");
+        foreach (var items in myArrList)
+        {
+            Console.WriteLine(items);
+        }
+    }
+}

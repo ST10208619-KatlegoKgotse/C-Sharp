bdc8434c2f100b7ab56a180192586f7fa2c8f957
Katlego Kgotse <st10208619@rcconnect.edu.za>
2023-05-25 13:59:07 +0000

    added data from microsoft

---
+++ Example/Example/Program.cs
@@ -0,0 +1,20 @@
+﻿using Example;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Create a list of parts.
+        List<Parts> parts = new List<Parts>();
+
+        // Add parts to the list.
+        parts.Add(new Parts() { PartName = Console.ReadLine(), PartId = Convert.ToInt32(Console.ReadLine()) });
+
+        // Write out the parts in the list. This will call the overridden ToString method
+        // in the Part class.
+        Console.WriteLine();
+        foreach (Parts aPart in parts)
+        {
+            Console.WriteLine(aPart);
+        }  
+    }
+}
